d3de4ba633e289bb15b3d41e2df4a9a207c42105
AmundReierstad <89453041+AmundReierstad@users.noreply.github.com>
2023-11-10 11:06:49 +0100

    Further on genetic algo

--- GEA_folderTask/Assets/GeneticAlgo.cs
+++ GEA_folderTask/Assets/GeneticAlgo.cs
@@ -1,17 +1,67 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public class GeneticAlgo : MonoBehaviour
 {
-    [SerializeField]private float populationMin = -1f;
-    [SerializeField] private float populationMax = 1f;
+    [SerializeField]private float populationMinParameterValue = -1f;
+    [SerializeField] private float populationMaxParameterValue = 1f;
     [SerializeField] private float crossProbability = 0.6f;
     [SerializeField] private float mutateProbability = 0.3f;
     [SerializeField] private float mutateAmount = 2f;
     [SerializeField] private float mutationPercentage = 2f;
-    
-    
+    private static Random _random = new Random();
+    private List<Genome> _currentPopulation;
+    //shorthand declaration of fields
+    public uint PopulationSize
+    {
+        get;
+        private set;
+    }
+    public uint GenerationCount
+    {
+        get;
+        private set;
+    }
+    public bool SortPopulationBeforeDestruction
+    {
+        get;
+        private set;
+    }
+    public bool Running
+    {
+        get;
+        private set;
+    }
+    //events
+    public event System.Action<GeneticAlgo> AlgorithmEnded;
+    public event System.Action<IEnumerable<Genome>> FitnessCalculationFinished;
+    //methods
+    public GeneticAlgo(int genomeParameterCount, int populationSize)
+    {
+        PopulationSize = (uint)populationSize;
+        _currentPopulation = new List<Genome>(populationSize);
+        for (int i = 0; i < populationSize; i++)
+        {
+            _currentPopulation.Add(new Genome(new List<float>(genomeParameterCount)));
+        }
+    }
+
+    public void PopulationInitialisation(IEnumerable<Genome> population)
+    {
+        //Set parameters to random values in set range
+        foreach (Genome genome in population)
+            genome.SetRandomParameters(populationMinParameterValue, populationMaxParameterValue);
+    }
+    public void Start()
+    {
+        PopulationInitialisation(_currentPopulation);
+        
+        Running = true;
+        
+    }
     
     
 }
--- GEA_folderTask/Assets/Genome.cs
+++ GEA_folderTask/Assets/Genome.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
@@ -5,7 +6,7 @@ using System.IO;
 using UnityEngine;
 using Random = System.Random;
 
-public class Genome : MonoBehaviour, IEnumerable
+public class Genome :  IEnumerable
 {
   private Random _randomizer = new Random();
   // private Random randomizer = new Random();
@@ -51,7 +52,13 @@ public class Genome : MonoBehaviour, IEnumerable
       _parameters[i] = (float)((_randomizer.NextDouble() * range) + minVal);
     }
   }
-
+  public float[] GetParameterCopy()
+  {
+    float[] copy = new float[_parameters.Count];
+    for (int i = 0; i < _parameters.Count; i++)
+      copy[i] = _parameters[i];
+    return copy;
+  }
   public void SaveGenomeToFile(string filepath)
   {
     StringBuilder builder = new StringBuilder();
@@ -61,6 +68,33 @@ public class Genome : MonoBehaviour, IEnumerable
     File.WriteAllText(filepath,builder.ToString());
   }
   
+  public static Genome GenerateRandom(int parameterCount, float minValue, float maxValue)
+  {
+    //Check arguments
+    if (parameterCount == 0) return new Genome(new List<float>(0));
+
+    Genome randomGenome = new Genome(new List<float>(parameterCount));
+    randomGenome.SetRandomParameters(minValue, maxValue);
+
+    return randomGenome;
+  }
+
   
+  public static Genome LoadFromFile(string filePath)
+  {
+    string data = File.ReadAllText(filePath);
+
+    List<float> parameters = new List<float>();
+    string[] paramStrings = data.Split(';');
+
+    foreach (string parameter in paramStrings)
+    {
+      float parsed;
+      if (!float.TryParse(parameter, out parsed)) throw new ArgumentException("The file at given file path does not contain a valid genotype serialisation.");
+      parameters.Add(parsed);
+    }
+
+    return new Genome(parameters);
+  }
 }
 //sources:https://github.com/ArztSamuel/Applying_EANNs/tree/master  
